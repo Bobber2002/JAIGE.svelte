3e9cef7bf66f8dcd6877c2d89ba8c70934898649
Bobber2002 <christofferlangkilde07@hotmail.com>
2023-11-06 23:18:20 +0100

    add stat requirement possibility

--- src/lib/components/Action.svelte
+++ src/lib/components/Action.svelte
@@ -1,10 +1,11 @@
 <script>
-  import { DOING } from "$lib/Data.js";
+  import { DOING, STATS } from "$lib/Data.js";
   import { createEventDispatcher } from "svelte";
 
   import ProgressBar from "./ProgressBar.svelte";
 
   export let data = new Object();
+  export let unlocked = false;
 
   let Active = $DOING.Doing.Name == data.Name && true;
   $: Active = $DOING.Doing.Name == data.Name && true;
@@ -14,32 +15,54 @@
 
 <!-- svelte-ignore a11y-click-events-have-key-events -->
 <!-- svelte-ignore a11y-no-static-element-interactions -->
-<div
-  id={data.Name}
-  class="bg-primary flex flex-col h-full w-full rounded-lg overflow-hidden text-gray-100 transition-transform hover:transform hover:cursor-pointer hover:scale-105 hover:shadow-[0_0_8px_6px_rgba(255,255,255,.25)]"
-  on:click={() => {
-    DOING.set({ ...data });
-    DOING.resetData();
-    dispatch("actionSet");
-  }}
->
+{#if unlocked}
+  <!-- content here -->
   <div
-    class="h-1/5 bg-black bg-opacity-20 flex px-4 gap-2 shadow-[0px_0px_12px_0px_rgba(255,255,255,.25)]"
+    id={data.Name}
+    class="bg-primary flex flex-col h-full w-full rounded-lg overflow-hidden text-gray-100 transition-transform hover:transform hover:cursor-pointer hover:scale-105 hover:shadow-[0_0_8px_6px_rgba(255,255,255,.25)]"
+    on:click={() => {
+      DOING.set({ ...data });
+      DOING.resetData();
+      dispatch("actionSet");
+    }}
   >
-    <h3 class="grow flex items-center text-xl whitespace-nowrap font-bold">
-      {data.Name}
-    </h3>
-    <h4 class="flex items-center justify-end italic">
-      2xp / {data.Time}s
-    </h4>
+    <div
+      class="h-1/5 bg-black bg-opacity-20 flex px-4 gap-2 shadow-[0px_0px_12px_0px_rgba(255,255,255,.25)]"
+    >
+      <h3 class="grow flex items-center text-xl whitespace-nowrap font-bold">
+        {data.Name}
+      </h3>
+      <h4 class="flex items-center justify-end italic">
+        2xp / {data.Time}s
+      </h4>
+    </div>
+    <div class="grow flex justify-between items-center p-4">
+      <img
+        src={data.Image}
+        alt={data.Name + " image"}
+        draggable="false"
+        class="w-24 aspect-auto object-cover"
+      />
+    </div>
+    <ProgressBar Time={data.Time} {Active} />
   </div>
-  <div class="grow flex justify-between items-center p-4">
-    <img
-      src={data.Image}
-      alt={data.Name + " image"}
-      draggable="false"
-      class="w-24 aspect-auto object-cover"
-    />
+{:else}
+  <!-- content here -->
+  <div
+    id={data.Name}
+    class="after:absolute after:w-full after:h-full after:bg-black after:bg-opacity-60 relative bg-primary flex flex-col items-center h-full w-full rounded-lg overflow-hidden text-gray-100 transition-transform hover:transform hover:cursor-pointer hover:scale-105 hover:shadow-[0_0_8px_6px_rgba(255,255,255,.25)]"
+  >
+    <div
+      class="h-1/5 bg-black bg-opacity-20 w-full flex px-4 gap-2 shadow-[0px_0px_12px_0px_rgba(255,255,255,.25)]"
+    >
+      <h3 class="grow flex items-center text-xl whitespace-nowrap font-bold">
+        Locked
+      </h3>
+    </div>
+    {#each data.Requirements.Stats as stat}
+      <p class="text-dark font-black z-10 grow flex items-center">
+        {stat.Stat}: {$STATS[stat.Stat].Level} / {stat.Amount}
+      </p>
+    {/each}
   </div>
-  <ProgressBar Time={data.Time} {Active} />
-</div>
+{/if}
--- src/lib/components/Page.svelte
+++ src/lib/components/Page.svelte
@@ -1,5 +1,5 @@
 <script>
-  import { PAGE } from "$lib/Data.js";
+  import { PAGE, STATS } from "$lib/Data.js";
   import GameData from "$lib/Data.json";
   import { Action } from "$lib/index.js";
   import Player from "./Player.svelte";
@@ -11,7 +11,9 @@
   >
     {$PAGE}
   </h2>
-  <div class="h-full w-full py-4 phone:px-4 px-6 overflow-y-auto overflow-x-clip">
+  <div
+    class="h-full w-full py-4 phone:px-4 px-6 overflow-y-auto overflow-x-clip"
+  >
     {#if $PAGE == "Player"}
       <Player />
     {:else}
@@ -19,7 +21,13 @@
         class="grid place-items-center w-full min-h-full grow xl:grid-cols-[repeat(6,calc((100%-5rem)/6))] lg:grid-cols-[repeat(5,calc((100%-4rem)/5))] md:grid-cols-[repeat(4,calc((100%-3rem)/4))] sm:grid-cols-[repeat(3,calc((100%-2rem)/3))] phone:grid-cols-[repeat(2,calc((100%-1rem)/2))] auto-rows-[12rem] gap-4"
       >
         {#each GameData.Actions[$PAGE] as action}
-          <Action data={action} on:actionSet/>
+          {#each action.Requirements.Stats as statRequirement}
+            {#if $STATS[statRequirement.Stat].Level >= statRequirement.Amount}
+              <Action data={action} on:actionSet unlocked={true} />
+            {:else}
+              <Action data={action} on:actionSet unlocked={false} />
+            {/if}
+          {/each}
         {/each}
       </div>
     {/if}
